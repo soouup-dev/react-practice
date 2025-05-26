25df8eb9040b00b7ca13dea003a1963f51d84d46
souup <soouup93@gmail.com>
2025-05-26 13:54:05 +0900

    1. count1, count2 독립 실행 가능 2. useState로 count 관리 3. 같은 component로 독립 실행

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,19 @@
 import React, {useState} from 'react';
 
 function Counter({title}) {
+  const [count, setCount] = useState(0);
 
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+      <div style={{border: '1px solid gray', padding: '1rem', margin: '1rem'}}>
+        <h2>{title}</h2>
+        <p>현재 카운트: {count}</p>
+        <button onClick={handleClick}>+1 증가</button>
+      </div>
+  );
 }
+
+export default Counter;
